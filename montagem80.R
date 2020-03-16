adf8665a6189293ec8730163f34fc66769933a8f
gomesfellipe <gomes.fellipe@hotmail.com>
2020-03-16 02:11:12 -0300

    att report

---
+++ montagem80.R
@@ -0,0 +1,50 @@
+montagem80 <- function(variavel = NULL,
+                     fixa = NULL,
+                     entrada = NULL,
+                     idade = NULL,
+                     return = F){
+  
+  moeda_real <- function(x){
+    paste0("R$", format(x, big.mark = ".", decimal.mark = ",", nsmall = 2, digits = 2))
+  }
+  
+  # Definir percentual com regra dos 80:
+  perc_variavel <- (80 - idade)/100
+  perc_fixa <- 1 - perc_variavel
+  
+  if(!is.null(fixa)){ 
+    variavel = (perc_variavel*fixa)/perc_fixa
+  }
+  
+  if(!is.null(variavel)){
+    fixa = (perc_fixa * variavel) / perc_variavel
+  }
+  
+  if(!is.null(fixa)  & !is.null(variavel)){
+    entrada = fixa + variavel
+  }
+  
+  if(!is.null(entrada)){ 
+    fixa = entrada * perc_fixa 
+    variavel = entrada * perc_variavel
+  } 
+  
+  
+  if(return == T){
+    return(
+      c(entrada = entrada,
+        fixa = fixa,
+        variavel = variavel))
+  }else{
+    cat(paste0("• Entrada: R$",
+               moeda_real(entrada), "\n"))
+    cat(paste0("  └─ Renda fixa:     ",
+               moeda_real(fixa), "\n"))
+    cat(paste0("  └─ Renda variavel: ",
+               moeda_real(variavel), "\n"))
+    cat(paste0("       (Acao + Cryptomoeda): \n       ",
+               moeda_real(variavel*.9), " + ",
+               moeda_real(variavel*.1), "\n"))
+  }
+  
+}
